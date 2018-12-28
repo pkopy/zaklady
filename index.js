49ddc12c78e715a230fc0b23b9c8cfeb9618f521
pkopy <pawel.kopycki@gmail.com>
2018-12-28 15:59:24 +0100

    chenging bet

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const cors = require('cors');
 const db = require('./lib/db');
 const helpers = require('./lib/helpers');
 const _data = require('./lib/dataFs');
+const bet = require('./lib/bet')
 
 let session = []
 
@@ -216,72 +217,28 @@ app.post('/login', (req, res) => {
 
 //Required data: token, email, bet: id user, id match, bet
 app.post('/bet', (req, res) => {
-  req.on('data', (data) => {
-    let payload = Buffer.from(data).toString();
-    const payloadObject = helpers.parseJsonToObject(payload);
-    // const name = typeof (payloadObject.name) == 'string' && payloadObject.name.trim().length > 0 ? payloadObject.name.trim() : false;
-    const email = typeof (payloadObject.email) == 'string' && payloadObject.email.trim().length > 0  && /[\w+0-9._%+-]+@[\w+0-9.-]+\.[\w+]{2,3}/.test(payloadObject.email.trim()) ? payloadObject.email.trim() : false;
-    // const bets = typeof (payloadObject.bets) == 'object' && payloadObject.bets instanceof Array && payloadObject.length > 0 ? payloadObject.bets : false;
-    // const idUser = typeof(payloadObject.idUser) == 'string' && payloadObject.idUser.trim().length > 0 ? payloadObject.idUser.trim() : false;
-    const idMatch = typeof(payloadObject.idMatch) == 'number' && payloadObject.idMatch > 0 ? payloadObject.idMatch : false;
-    const bet = typeof(payloadObject.bet) == 'number' && [1, 2, 3].indexOf(payloadObject.bet) > -1 ? payloadObject.bet : false;
-  
-  
-    if (email && idMatch && bet) {
-      //Get the token from the headers
-      const token = typeof(req.headers.token) =='string' && req.headers.token.length == 20 ? req.headers.token : false;
-      // console.log(typeof(req.headers.token))
-      // let idUser = 0;
-      helpers.verifyToken(token, email, (tokenIsValid) => {
-        // console.log(helpers.parseJsonToObject(data))
-        if (tokenIsValid) {
-          db.read('user', 'email', email)
-          .then(userData => {
-            const userId = userData[0].id;
-            const dataBet = {
-              'id_user' : userId,
-              'id_match' : idMatch,
-              'bet' : bet
-            }
-            db.read('bet', 'id_user', userId)
-            .then(betData => {
-              let isBetExist = false
-              betData.forEach(bet => {
-                if (bet.id_user === userId && bet.id_match === idMatch) {
-                  isBetExist = true;
-                }
-              });
-
-              if (isBetExist) {
-                helpers.response(res, 403, {'Error' : 'this bet already exist'});
-              } else {
-                db.create('bet', dataBet)
-                .then(() => {
-                  helpers.response(res, 200, {'Message' : 'New bet has been added'});
-                })
-                .catch((err) => {
-                  helpers.response(res, 500, {'Error' : 'Could not add new bet'});
-                });
-
-              }
-            });
-          });
-          
-
-        } else {
-          helpers.response(res, 403, {'Error' : 'Missing required token in header, or token is invalid'});
-        }
-      });
-    } else {
-      helpers.response(res, 403, {'Error' : 'Missing required field(s)'});
-    }
-  })
-  
+  bet.post(req, res);
 });
 
 // app.get('/(\\w+)', (req, res) => {
 //   helpers.response(res, 400, {'Error' : 'Not Found'})
 // });
+
+match = (bets) => {
+  const arr =[]
+  return new Promise((res, rej) => {
+    bets.forEach(bet => {
+      db.read('match_', 'id', bet.id_match)
+      .then(data => {
+        bet.match = data 
+        arr.push(bet)
+      })
+      .catch(err => console.log(err))
+    })
+    res(arr)
+  }) 
+}
+
 
 //Required data: token, email
 app.get('/user', (req, res) => {
@@ -300,8 +257,12 @@ app.get('/user', (req, res) => {
           }
           db.read('bet', 'id_user', dataUser[0].id)
           .then(betsData => {
-            userObj.bets = betsData
-            helpers.response(res, 200, userObj)
+            match(betsData)
+            .then(data => {
+              userObj.bets = data
+              helpers.response(res, 200, userObj)
+            })
+            .catch(err => console.log(err))
           })
           .catch(() => {
             userObj.bets = []
